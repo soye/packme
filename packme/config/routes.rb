ebf59b57064a153c1b65ea368f2115f14118dc88
EmilyNguyen <emilynguyen567@gmail.com>
2015-02-21 02:19:12 -0800

    added views

--- packme/config/routes.rb
+++ packme/config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get ':controller(/:action(/:id))'
+  post ':controller(/:action(/:id))'
 end
